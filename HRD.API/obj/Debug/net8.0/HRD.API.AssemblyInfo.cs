0d600af432ab25074aa44a2bf2315f678b54d793
adityasuhendar <suhendaradityawahyu@gmail.com>
2025-07-15 12:18:38 +0700

    fix css presensi

--- HRD.API/obj/Debug/net8.0/HRD.API.AssemblyInfo.cs
+++ HRD.API/obj/Debug/net8.0/HRD.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRD.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9873faefb2df35cf2a9f7031c995e463151c49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff39126ab61b4e3ddc8e9330c52ffbc4235cd648")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRD.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRD.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
